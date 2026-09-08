77eaefbf26b618929da14e356b51b231b741c4d8
Devs <dblanaru41@gmail.com>
2026-09-08 11:58:50 +0200

    First Push

---
+++ My project/Assets/Scripts/Play.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Play : MonoBehaviour
+{
+    public void NextScene()
+    {
+        SceneManager.LoadScene("Game");
+    }
+    void Start()
+    {
+
+    }
+    void Update()
+    {
+
+    }
+
+}
